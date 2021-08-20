2fd41c7a4133531462dc0fda6b67084756cf082f
Sajjan <iamsajjanshahwork@gmail.com>
2021-08-20 15:27:18 +0530

    about hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+let App = () => {
+
+    let [count, setCount] = useState(0);
+    let [process, setProcess] = useState("processing")
+
+    console.log("render was called");
+
+    //case 1:
+    //useEffect takes two argument 1-function 2-array
+    //if function is passed with empty array then useEffect will act as component did mopunt 
+    // will only get executed once,the component is rendered
+    // useEffect(()=>{
+    //   console.log("useEffect was called");
+    // },[])
+
+    //case 2:
+    //if only function is passed with no array then after every render useEffect will be called
+    // useEffect(()=>{
+    //   console.log("useEffect Case 2 was called");
+    // })
+
+
+    //case 3:  
+
+
+    useEffect(()=>{
+      let arr = process.split("i")
+      console.log(arr)
+    },[process])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick = {()=> {
+        setCount(count+1)
+      }}> + </button>
+      <p>{count}</p>
+      <button onClick = {()=> {
+        setCount(count-1)
+      }}>-</button>
+
+
+      <p>{process}</p>
+      <button onClick = {()=>{
+        setProcess("sucess");
+      }}>Click Me</button>
     </div>
   );
 }
